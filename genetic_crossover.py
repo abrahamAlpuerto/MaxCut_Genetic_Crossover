633ea66d9b7e6ebe38a91e5fcb324de3f311ec92
Abraham Alpuerto <aalpuertoiii@gmail.com>
2024-04-28 21:13:11 -0400

    Add a multi Parent Genetic Crossover to further Search for local maximums related to parents.

---
+++ genetic_crossover.py
@@ -0,0 +1,65 @@
+import sys
+import copy
+import time
+import math
+from typing import List, Union
+import numpy as np
+import random
+import networkx as nx
+
+from tabu_Search import generate_random_population
+from tabu_Search import tabu_search
+from util import read_nxgraph
+from util import obj_maxcut
+
+def cross_over(population):
+    selected_parents = random.sample(population, num_parents)
+
+    child = []
+    for node in range(0,len(selected_parents[0])):
+        node_in_same_set = all(parent[node] == selected_parents[0][node] for parent in selected_parents)
+
+        if node_in_same_set:
+            child.append(selected_parents[0][node])
+        else:
+            child.append(random.radiant(0,1))
+    child, child_score = tabu_search(child,graph)
+    return child
+
+def algorithm_run():
+    population, best_binary_vector, best_score, population_scores = generate_random_population(graph, 10)
+    c_iter = 0
+    while c_iter < c_itMax:
+        child = cross_over(population)
+        child_score = obj_maxcut(child,graph)
+        print("Childs Score: ", child_score)
+    
+        # Finding the min score in the list and replacing it with the child if smaller than child cut
+        min_score_index = np.argmin(population_scores)
+        if(population_scores[min_score_index] < child_score):
+            population_scores[min_score_index] = child_score
+            population[min_score_index] = child
+        c_iter += 1
+
+    max_score_index = np.argmax(population_scores)
+    max_score_vector = population[max_score_index]
+    print("Binary Vector: ", max_score_vector)
+    print("Score of Cut: ", population_scores[max_score_index])
+    print("Genetic Search Complete")
+
+
+    
+if __name__ == '__main__':
+    # Constants
+    num_parents = 4
+    c_itMax = 4
+    # read data
+    graph = read_nxgraph('./data/gset/gset_14.txt')
+    print("Genetic Search Start")
+    algorithm_run()
+
+    # Cut checker
+    # vector = [1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1]
+    # print(obj_maxcut(vector,graph))
+
+ 
--- tabu_Search.py
+++ tabu_Search.py
@@ -9,6 +9,11 @@ import networkx as nx
 from util import read_nxgraph
 from util import obj_maxcut
 
+#constants for tabuSearch
+P_iter = 150
+MaxIter = 100000
+gamma = 60
+
 def generate_random(graph):
     nodes = list(graph.nodes())
     binary_vector = [random.randint(0, 1) for _ in range(len(nodes))]
@@ -18,18 +23,21 @@ def generate_random_population(graph, pop_size):
     count = 1
     Pop = []
     best_binary_vector = []
+    score_list = []
     best_score = 0
     while len(Pop) < pop_size:
-        result = generate_random(graph)
-        binary_vector, score = result
+        binary_vector, result = generate_random(graph)
+        score = result
+
         if binary_vector not in Pop:
+            score_list.append(score)
             Pop.append(binary_vector)
             print(count, "Score: " , score)
             count += 1
             if score > best_score:
                 best_score = score
                 best_binary_vector = binary_vector
-    return Pop, best_binary_vector, best_score
+    return Pop, best_binary_vector, best_score, score_list
 
 
 def tenure(iteration, maxT):
@@ -88,7 +96,7 @@ def tabu_search(initial_solution, graph):
     
     # Initialize tabu list and tabu tenure
     tabu_list = [0] * len(curr_solution)
-    maxT = 20  
+    maxT = 100
     
     # Compute move gains
     move_gains = compute_move_gains(graph, curr_solution, tabu_list)
@@ -132,33 +140,27 @@ def tabu_search(initial_solution, graph):
 
 
 
-def memetic_algorithm(graph):
-    return
+# if __name__ == '__main__':
+#     # read data
+#     graph = read_nxgraph('./data/gset/gset_14.txt')
+    # # vector = [1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1]
+    # # print(obj_maxcut(vector,graph))
+    # # generating random solutions
+    # print("Tabuu Search")
+    # population, best_binary_vector, best_score = generate_random_population(graph, 10)
+    # print("Best Binary Vector:", best_binary_vector)
+    # print("Best Score:", best_score)
+    # total = 0
+    # for i in population:
+    #     total += obj_maxcut(i,graph)
+    # total /= 10
+    # print("Average of Population: ", total)
 
-if __name__ == '__main__':
-    # read data
-    graph = read_nxgraph('./data/gset/gset_14.txt')
-    P_iter = 200
-    MaxIter = 1000000
-    gamma = 75
-    # vector = [1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1]
-    # print(obj_maxcut(vector,graph))
-    # generating random solutions
-    print("Tabuu Search")
-    population, best_binary_vector, best_score = generate_random_population(graph, 10)
-    print("Best Binary Vector:", best_binary_vector)
-    print("Best Score:", best_score)
-    total = 0
-    for i in population:
-        total += obj_maxcut(i,graph)
-    total /= 10
-    print("Average of Population: ", total)
-
-    while True:
-        population, new_binary_vector, new_score = generate_random_population(graph, 10)
-        if new_score > best_score:
-            best_binary_vector = copy.deepcopy(new_binary_vector)
-            best_score = new_score
-            print("Best Binary Vector:", best_binary_vector)
-            print("Best Score:", best_score)
+    # while True:
+    #     population, new_binary_vector, new_score = generate_random_population(graph, 10)
+    #     if new_score > best_score:
+    #         best_binary_vector = copy.deepcopy(new_binary_vector)
+    #         best_score = new_score
+    #         print("Best Binary Vector:", best_binary_vector)
+    #         print("Best Score:", best_score)
 
